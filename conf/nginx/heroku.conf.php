da2d46b56f24834f78b7b8569ab73577a1ec9a31
nosovk <nosov@nodeart.io>
2019-09-22 23:42:47 +0300

    Handle OPTION requests in nginx Do note rate limit them

--- conf/nginx/heroku.conf.php
+++ conf/nginx/heroku.conf.php
@@ -27,11 +27,11 @@ http {
 	# Depending on your set-up you set more restrictive trusted proxies:
 	set_real_ip_from 0.0.0.0/0;
 
-	add_header X-debug-enabled "true" always;
-	add_header X-debug-message $http_x_forwarded_for always;
-	add_header X-debug-message-cloudflare $http_cf_connecting_ip always;
+#	add_header X-debug-enabled "true" always;
+#	add_header X-debug-message $http_x_forwarded_for always;
+#	add_header X-debug-message-cloudflare $http_cf_connecting_ip always;
 
-	# define an easy to reference name that can be used in fastgi_pass
+# define an easy to reference name that can be used in fastgi_pass
 	upstream heroku-fcgi {
 		#server 127.0.0.1:4999 max_fails=3 fail_timeout=3s;
 		server unix:/tmp/heroku.fcgi.<?=getenv('PORT')?:'8080'?>.sock max_fails=3 fail_timeout=3s;
@@ -78,7 +78,19 @@ http {
 
 		# default handling of .php
 		location ~ \.php {
-                        limit_req       zone=phpapi  burst=3 nodelay;
+            # handle all OPTIONS request with immediate end of processing
+            if ($request_method = 'OPTIONS') {
+                add_header 'Access-Control-Allow-Origin' '*';
+                add_header 'access-control-expose-headers' 'Content-Disposition';
+                add_header 'Access-Control-Allow-Methods' 'POST, PUT, GET, DELETE, OPTIONS';
+                add_header 'Access-Control-Allow-Headers' '*';
+
+                add_header 'Access-Control-Max-Age' 1728000;
+                add_header 'Content-Type' 'text/plain; charset=utf-8';
+                add_header 'Content-Length' 0;
+                return 204;
+            }
+            limit_req       zone=phpapi  burst=3 nodelay;
 			try_files @heroku-fcgi @heroku-fcgi;
 		}
 	}
